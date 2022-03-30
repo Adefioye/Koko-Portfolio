a04b1bf8f5d3e79a820b493a3ccb087c7a206547
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-03-30 09:10:57 +0100

    adds menu bar with mobile responsiveness

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 import { images } from "../../constants";
 
 import "./Navbar.scss";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   const navTabs = ["home", "about", "work", "skills", "contact"];
+
   return (
     <nav className="app__navbar">
+      {/* Logo section */}
       <div className="app__navbar-logo">
         <img src={images.logo} alt="logo" />
       </div>
+
+      {/* Section on navigation tabbs on desktop */}
       <ul className="app__navbar-links">
         {navTabs.map((item) => (
           <li key={`link-${item}`} className="app__flex p-text">
@@ -19,7 +27,30 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
-    </nav> 
+
+      {/* Section on navigation tabbs on mobile(small screens) */}
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {navTabs.map((item) => (
+                <li key={`link-${item}`}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
+    </nav>
   );
 };
 
