82ff8cbb6d854ef50e3cf62b58af2fb6f12204af
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-04-03 14:18:35 +0100

    added motionwrap to sections from about to skills

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
 
-import { AppWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 import "./Work.scss";
 
@@ -19,17 +19,14 @@ const Work = () => {
     setAnimateCard([{ y: 100, opacity: 0 }]);
 
     setTimeout(() => {
-      setAnimateCard([{y: 0, opacity: 1}])
+      setAnimateCard([{ y: 0, opacity: 1 }]);
 
       if (item === "All") {
-        setFilterWork(works)
+        setFilterWork(works);
       } else {
-        setFilterWork(works.filter(work => work.tags.includes(item)))
+        setFilterWork(works.filter((work) => work.tags.includes(item)));
       }
-
-
-    }, 500)
-
+    }, 500);
   };
 
   useEffect(() => {
@@ -125,4 +122,8 @@ const Work = () => {
   );
 };
 
-export default AppWrap(Work, "work");
+export default AppWrap(
+  MotionWrap(Work, "app__works"),
+  "work",
+  "app__primarybg"
+);
